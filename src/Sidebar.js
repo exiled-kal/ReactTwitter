afc63e613b0a1933d1ba6100efffd7a18e5fe04c
kaldne <tseringkalden1964@gmail.com>
2020-08-09 15:56:42 -0700

    initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter */}
+      <TwitterIcon />
+
+      {/* SidebarOption */}
+
+      {/* Button -> Tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
